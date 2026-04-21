e237c29a9f85ebc98ce77c0ca452d52d26bd3405
Gsr99 <ganeshsreddy1@gmail.com>
2026-04-21 13:56:54 +0200

    Add Firebase configuration and initial project setup files

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FlowHub from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<FlowHub />)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
